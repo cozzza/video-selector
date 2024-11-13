d1bd1d8f0a51656c5ae3329e48cc758be8f673ea
bean <bean@192.168.1.11>
2024-11-13 13:41:03 -0300

    Initial commit

---
+++ src/VideoSelector.js
@@ -0,0 +1,70 @@
+import { useState, useRef, useEffect } from 'react';
+import { frames, audios } from './arrays';
+import './styles.css';
+import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
+import GraphicEqIcon from '@mui/icons-material/GraphicEq';
+import EqualizerIcon from '@mui/icons-material/Equalizer';
+
+export const VideoSelector = () => {
+  const [audioList, setAudioList] = useState([]);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentAudioIndex, setCurrentAudioIndex] = useState(0);
+  const audioRef = useRef(new Audio());
+
+  const handleAddToQueue = (index) => {
+    setAudioList((prev) => [...prev, index]);
+  };
+
+  const playAudioQueue = () => {
+    if (audioList.length > 0 && currentAudioIndex < audioList.length) {
+      const audioKey = Object.keys(audios)[audioList[currentAudioIndex]];
+      audioRef.current.src = audios[audioKey];
+      audioRef.current.play();
+      setIsPlaying(true);
+
+      audioRef.current.onended = () => {
+        setIsPlaying(false);
+        setCurrentAudioIndex((prevIndex) => prevIndex + 1);
+      };
+    } else {
+      setIsPlaying(false);
+      setCurrentAudioIndex(0);
+    }
+  };
+  
+  useEffect(() => {
+    if (!isPlaying && currentAudioIndex < audioList.length) {
+      playAudioQueue();
+    }
+  }, [currentAudioIndex, audioList, isPlaying]);
+
+  return (
+    <div>
+      <div>
+        <h1 style={{ color: 'white' }}>Un cuerpo expuesto</h1>
+        <h3 style={{ color: 'white' }}>Selecciona un sonido</h3>
+      </div>
+      <div className="video-grid">
+        {frames.map((frame, index) => (
+          <div key={index} className="video-card" onClick={() => handleAddToQueue(index)}>
+            <img src={frame} alt={`Frame ${index}`} />
+            <div >
+            {audioList[currentAudioIndex] === index && isPlaying ? (
+              <div className="equalizer-bar-container">
+                <div className="equalizer-bar"></div>
+                <div className="equalizer-bar"></div>
+                <div className="equalizer-bar"></div>
+              </div>
+              ) : (
+                <PlayCircleOutlineIcon fontSize="large" className="icon-overlay" /> )} 
+
+            {/* */}
+
+
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
---
+++ src/arrays.js
@@ -0,0 +1,36 @@
+import abdominales from './frames/abdominales.png'
+import mandibula from './frames/mandibula.png';
+import pelota from './frames/pelota.png';
+import tabaco from './frames/tabaco.png';
+import makeup from './frames/makeup.png';
+import comoPosar from './frames/comoPosar.png';
+import guitarra from './frames/guitarra.png';
+import saltar from './frames/saltar.jpeg';
+
+import abdominales_1 from './AUDIOS/abdominales_1.mp3';
+import mandibula1 from './AUDIOS/mandibula1.mp3';
+import jueguitos from './AUDIOS/jueguitos.mp3';
+import saltar_cuerda from './AUDIOS/saltar_cuerda.mp3';
+import emiliamakeup_2 from './AUDIOS/emiliamakeup_2.mp3';
+import cigarrillo from './AUDIOS/cigarrillo.mp3';
+import como_posar2 from './AUDIOS/como_posar2.mp3';
+
+export const frames = [
+  abdominales,
+  mandibula,
+  pelota,
+  tabaco,
+  makeup,
+  comoPosar,
+  saltar
+];
+
+export const audios = {
+  abdominales_1,
+  mandibula1,
+  jueguitos,
+  cigarrillo,
+  emiliamakeup_2,
+  como_posar2,
+  saltar_cuerda
+}
